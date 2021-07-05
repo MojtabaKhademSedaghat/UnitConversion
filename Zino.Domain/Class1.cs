a46fd8f24d154be71dec5dc96e59afa64460d57b
Mojtaba7 <m.khsedaghat@gmail.com>
2021-07-05 18:02:16 +0430

    first Commit

---
+++ Zino.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Zino.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Zino.Domain/SeedWork/Enumeration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zino.Domain.SeedWork
+{
+    class Enumeration
+    {
+    }
+}
